1e54b941ea4a63f2fbc911b16a7ade54099b2572
hader zayed <haderzayed55@gmail.com>
2021-08-25 19:26:02 +0200

    edit by hader

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,11 +16,11 @@ class CategoryController extends Controller
 
     public function show($name){
         $users = User::join('categories', 'users.category_id', '=', 'categories.id')
-        ->join('reviews' , 'users.id' , '=' , 'reviews.ratee_id')
-        ->select('users.*', 'categories.name as categeroyName ','reviews.rate as reviewsrate' )
+         
+        ->select('users.*', 'categories.name as categeroyName ' )
         ->where('users.type','developer')
         ->where('categories.name' ,$name)
-        ->orderBy('reviews.rate', 'DESC')
+          
         ->get();
 
         return $users  ;
