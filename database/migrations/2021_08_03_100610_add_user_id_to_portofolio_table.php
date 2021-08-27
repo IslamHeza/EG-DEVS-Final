1f8ffb996473556f6ea8ba00bfa903086d878f02
NermeenShehab <Nermeenshehab490@gmail.com>
2021-08-27 03:40:22 +0200

    work on logic

--- database/migrations/2021_08_03_100610_add_user_id_to_portofolio_table.php
+++ database/migrations/2021_08_03_100610_add_user_id_to_portofolio_table.php
@@ -12,12 +12,12 @@ class AddUserIdToPortofolioTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::table('portfolios', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+            {
+        // Schema::table('portfolios', function (Blueprint $table) {
+        //     $table->unsignedBigInteger('user_id');
 
-             $table->foreign('user_id')->references('id')->on('users');
-        });
+        //      $table->foreign('user_id')->references('id')->on('users');
+        // });
     }
 
     /**
@@ -27,8 +27,10 @@ class AddUserIdToPortofolioTable extends Migration
      */
     public function down()
     {
-        Schema::table('portfolios', function (Blueprint $table) {
-            //
-        });
+        // Schema::table('portfolios', function (Blueprint $table) {
+        //     //
+        // });
     }
+
 }
+
